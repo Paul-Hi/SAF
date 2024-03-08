6e79631e6a02e8d32a99a16c82ecacd2cd423c39
Paul-Hi <32524715+Paul-Hi@users.noreply.github.com>
2024-03-08 14:07:41 +0100

    image bug

--- saf/core/image.cpp
+++ saf/core/image.cpp
@@ -86,6 +86,24 @@ void Image::update(VkPhysicalDevice physicalDevice, VkDevice logicalDevice, VkQu
         mWidth  = width;
         mHeight = height;
         release();
+
+        VkImageCreateInfo imageCreateInfo{};
+        imageCreateInfo.sType         = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+        imageCreateInfo.imageType     = VK_IMAGE_TYPE_2D;
+        imageCreateInfo.extent        = { width, height, 1 };
+        imageCreateInfo.mipLevels     = 1;
+        imageCreateInfo.arrayLayers   = 1;
+        imageCreateInfo.format        = format;
+        imageCreateInfo.tiling        = VK_IMAGE_TILING_OPTIMAL;
+        imageCreateInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+        imageCreateInfo.usage         = VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT;
+        imageCreateInfo.sharingMode   = VK_SHARING_MODE_EXCLUSIVE;
+        imageCreateInfo.samples       = VK_SAMPLE_COUNT_1_BIT;
+        imageCreateInfo.flags         = 0;
+
+        VkResult err = vkCreateImage(logicalDevice, &imageCreateInfo, nullptr, &mImage);
+        checkVkResult(err);
+
         allocateMemory(physicalDevice, logicalDevice, queue, commandPool, commandBuffer);
     }
 
